b46bf2119fdeefc5747d209524b1b21898b0632f
Bryan Chen <mche618@aucklanduni.ac.nz>
2023-07-08 23:38:18 +1000

    complete the ai response api with firestore integration

--- routes/ai_api.py
+++ routes/ai_api.py
@@ -2,8 +2,20 @@
 import uuid
 from datetime import datetime, timedelta
 from flask import jsonify, abort, request, Blueprint
+import os
+from datetime import datetime
+import time
+from zoneinfo import ZoneInfo
+import shortuuid
+import openai
+from dotenv import load_dotenv, find_dotenv
 
 from firebase_db_util import FirebaseConnection
+from ai.agents import generate_single_interview_response
+
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 AI_API = Blueprint('ai_api', __name__)
 db_connection = FirebaseConnection(base_collection='interviews')
@@ -26,13 +38,46 @@ def get_ai_reponse():
         abort(400)
     payload = request.get_json(force=True)
     session_id = payload["session_id"]
-    result = db_connection.find_one(session_id)
-    data = result._data
+    interview_db_obj = db_connection.find_one(session_id)
+    interview_session = interview_db_obj._data
 
-    print(result)
+    # check if session exist.
+    if not interview_session:
+        return jsonify({"response": "session does not exist."}), 400
+    interviewee = interview_session.get("interviewee", "annonymous user")
+    conversation = interview_session["conversation"]
 
-    # HTTP 201 Created
-    return jsonify({"response": "hello world"}), 201
+    # check if session has been completed.
+    completion = interview_session.get("completion", True)
+    if completion:
+        return jsonify({"response": "session has been completed."}), 400
+
+    # check if interview plan exist.
+    plan_id = interview_session["planId"]
+    plan_db_obj = db_connection.find_one(plan_id, "plans")
+    plan = plan_db_obj._data
+    if not plan:
+        return jsonify({"response": "interview plan does not exist."}), 400
+    agent_name = plan["agent name"]
+
+    # generate agent response
+    agent_response, signal_completion = generate_single_interview_response(agent_name, plan, conversation)
+    if agent_response:
+        new_utterance = {
+            "messageId": shortuuid.ShortUUID().random(length=8),
+            "isAI": True,
+            "message": agent_response,
+            "speaker": agent_name,
+            "time": datetime.now(tz=ZoneInfo('Australia/Sydney'))
+        }
+        interview_session["conversation"].append(new_utterance)
+        interview_session["conversation"] = [c for c in interview_session["conversation"] if c != ""]
+        db_connection.insert(interview_session, collection="interviews", doc_id=session_id, mode='set')
+
+        # HTTP 201 Created
+        return jsonify({"response": "response gene"}), 201
+    else:
+        return jsonify({"response": "something went wrong, the response generation was not completed"}), 400
 
 
 
